9f7ea1390530bb42ee1c1036c852365775bdac95
Kyle Sonlin <kyle@sonlin.dev>
2025-05-10 19:13:48 -0400

    refactor: move Providers to components directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import { SessionProvider } from 'next-auth/react'
-import Providers from '@/app/Providers'
+import Providers from '@/components/Providers'
 
 export const metadata: Metadata = {
   title: 'Contact Management System',
